3c4c771aa489443be99762681e154400d070eb60
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-05 10:51:03 +0700

    Create main.cpp

---
+++ Duyệt trâu - Tham lam/Calculating Function/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+long long n;
+
+int main()
+{
+  cin >> n;
+    if(n % 2 == 0)
+        cout << n/2;
+    else
+        cout << (n/2+1)*-1;
+  return 0;
+}
