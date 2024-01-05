e7d3650fe06a08f0561e0168051d19691039e713
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-05 09:36:59 +0700

    Create main.cpp

---
+++ Duyệt trâu - Tham lam/Stones on the Table/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string s;
+int n, cnt = 0;
+
+signed main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> s;
+    for(int i = 0; i < n; ++i)
+    {
+        if(s[i] == s[i+1])
+        {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
