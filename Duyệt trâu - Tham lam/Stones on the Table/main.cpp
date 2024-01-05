1001a73f94806e2a2c7d0de2f4f951289495ece3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-05 09:38:29 +0700

    Update main.cpp

--- Duyệt trâu - Tham lam/Stones on the Table/main.cpp
+++ Duyệt trâu - Tham lam/Stones on the Table/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 using namespace std;
 
@@ -9,14 +8,14 @@ int n, cnt = 0;
 signed main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n >> s;
-    for(int i = 0; i < n; ++i)
-    {
-        if(s[i] == s[i+1])
-        {
-            cnt++;
-        }
-    }
-    cout << cnt;
-    return 0;
+	cin >> n >> s;
+	for(int i = 0; i < n; ++i)
+	{
+		if(s[i] == s[i+1])
+		{
+			cnt++;
+		}
+	}
+	cout << cnt;
+	return 0;
 }
