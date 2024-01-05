57a43faf1dc72a1fdf80c66877c9e5988f747dda
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-05 10:52:55 +0700

    Create main.cpp

---
+++ Duyệt trâu - Tham lam/Ultra_Fast Mathematician/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string s1 , s2;
+int main()
+{
+  cin >> s1 >> s2;
+  for(int i = 0 ; i < s1.size(); i++)
+  {
+    if(s1[i] == s2[i])
+    {
+      cout << 0;
+    }
+    else cout << 1;
+  }
+  cout << endl;
+  return 0;
+}
