44a09545a835bd1e9a6f462c99efd808518e6fe0
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-06 12:01:29 +0700

    Update main.cpp

--- Deque_Stack/Biểu diễn trong hệ cơ số k/main.cpp
+++ Deque_Stack/Biểu diễn trong hệ cơ số k/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 #define int long long
+int n, k;
+stack <int> st;
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+    cin >> n >> k;
+    while(n)
+    {
+        st.push(n % k);
+        n /= k;
+    }
+    while(!st.empty())
+    {
+        cout << st.top();
+        st.pop();
+    }
     return 0;
 }
