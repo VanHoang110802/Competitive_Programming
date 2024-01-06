1b7db9d259fd9b03527b6e5ed829dbe135b9ca17
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-06 15:32:37 +0700

    Update main.cpp

--- Deque_Stack/MINmin/main.cpp
+++ Deque_Stack/MINmin/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
-#define int long long
+int x;
+string n, s;
 
-signed main()
+int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+    cin >> x >> s;
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(s[i] >= '0' && s[i] <= '9')
+            n += s[i];
+    }
+    int k = n.size() - x;
+    stack<int> digits;
+    for (long i = 0; i < n.length(); i++)
+    {
+        int tmp =  n[i] - 0;
+        if (digits.empty()) digits.push(tmp);
+        else 
+        {
+            while (!digits.empty() && tmp < digits.top() && k > 0) 
+            {
+                digits.pop();
+                k--;
+            }
+            digits.push(tmp);
+        }
+    }
+    while (k > 0 && !digits.empty()) 
+    {
+        digits.pop();
+        k--;
+    }
+    vector<char> chars;
+    while (!digits.empty()) 
+    {
+        int tmp = digits.top();
+        digits.pop();
+        chars.push_back(tmp);
+    }
+    for (long i = chars.size() - 1; i >= 0; i--) cout << chars[i];
     return 0;
 }
