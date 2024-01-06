5c10bb060c98ff23746c125e474c0eff9aee85ec
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-06 14:20:39 +0700

    Update main.cpp

--- Deque_Stack/Xóa số DELNUM/main.cpp
+++ Deque_Stack/Xóa số DELNUM/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#define int long long
+int n;
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
 
+    int ans = n;
+    while(1)
+    {
+        bool found = false;
+        for(int i = 1; i < a.size() - 1; i++)
+        {
+            if(a[i] < a[i - 1] && a[i] < a[i + 1])
+            {
+                a.erase(a.begin() + i);
+                ans--;
+                found = true;
+                break;
+            }
+        }
+        if (found == false) break;
+    }
+
+    cout << ans;
     return 0;
 }
