65d471aefc8b5fd5b288f030e8be3daaba3f98a3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-06 11:20:52 +0700

    Update main.cpp

--- Deque_Stack/Dãy ngoặc ver1/main.cpp
+++ Deque_Stack/Dãy ngoặc ver1/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
 
 #define int long long
+string s;
+stack <char> st;
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+    cin >> s;
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        if(s[i] == ')' && !st.empty())
+        {
+            st.pop();
+        }
+    }
+    if(st.empty()) cout << "YES";
+    else cout << "NO";
     return 0;
 }
