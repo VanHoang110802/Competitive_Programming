8d9704649e3a1836fd0cdc57ce91264af1df7766
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-07 14:00:25 +0700

    Update main.cpp

--- Duyệt trâu - Tham lam/ƯỚC CHUNG LỚN NHẤT/main.cpp
+++ Duyệt trâu - Tham lam/ƯỚC CHUNG LỚN NHẤT/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 #define int long long
@@ -7,19 +8,16 @@ int N, X, ans = 1;
 signed main() 
 {
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> N >> X;
-    if (N == 1)
-    {
-        cout << X;
-        return 0;
-    }
-    for (long long i = 1; pow(i, N) <= X; i++) 
-    {
-        if (X % (long long)pow(i, N) == 0)
-        {
-            ans = i;
-        }
-    }
-    cout << ans << '\n';
+  cin >> N >> X;
+  if (N == 1)
+  {
+    cout << X;
+    return 0;
+  }
+  for (int i = 1; pow(i, N) <= X; i++)
+  {
+    if (X % (int)pow(i, N) == 0) ans = i;
+  }
+  cout << ans << '\n';
   return 0;
 }
