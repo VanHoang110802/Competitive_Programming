6abff7edb495bdaf51715b92f984e2778852d5f8
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-09 12:53:45 +0700

    Update main.cpp

--- Hai con trỏ/Prepend and Append/main.cpp
+++ Hai con trỏ/Prepend and Append/main.cpp
@@ -12,18 +12,7 @@ signed main()
     //test = 1;
     while(test--)
     {
-        int n;
-        int a[MAXN];
-        cin >> n;
-        for(int i = 1; i <= n; ++i) cin >> a[i];
-        int l = 1, r = n;
-        while(l < r)
-        {
-            cout << a[l] << ' ' << a[r] << ' ';
-            ++l;
-            --r;
-        }
-        if(n % 2) cout << a[r];
+        
         cout << '\n';
     }
     return 0;
