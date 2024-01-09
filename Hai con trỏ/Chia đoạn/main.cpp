aad8edce0927bbeab8d2ad6143a6e200468b0c7c
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-09 13:42:24 +0700

    Create main.cpp

---
+++ Hai con trỏ/Chia đoạn/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+const int MAXN = 1e5;
+int n;
+int a[MAXN];
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  
+    return 0;
+}
