d14409e7b07e8ce0287d2d5f414c1fa0e6ce8074
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-09 13:47:54 +0700

    Update main.cpp

--- Hai con trỏ/Chia đoạn/main.cpp
+++ Hai con trỏ/Chia đoạn/main.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 const int MAXN = 1e5;
-int n;
-int a[MAXN];
+int n, s = 0, ans = 0;
+int a[MAXN], c[MAXN];
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  
+    cin >> n;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+        s += a[i];
+    }
+    if(s % 3 != 0)
+    {
+        cout << 0;
+    }
+    else
+    {
+        s /= 3;
+        int t = 0;
+        for(int i = n; i >= 0; --i)
+        {
+            t += a[i];
+            if(t == s)
+            {
+                c[i] = c[i + 1] + 1;
+            }
+            else
+            {
+                c[i] = c[i + 1];
+            }
+        }
+        t = 0;
+        for(int i = 1; i < n - 1; ++i)
+        {
+            t += a[i];
+            if(t == s) ans += c[i + 2];
+        }
+        cout << ans << '\n';
+    }
     return 0;
 }
