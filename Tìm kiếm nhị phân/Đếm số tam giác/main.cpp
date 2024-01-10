0cbb2df4d300468965b782e1cfc224ca52036003
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-10 13:00:58 +0700

    Update main.cpp

--- Tìm kiếm nhị phân/Đếm số tam giác/main.cpp
+++ Tìm kiếm nhị phân/Đếm số tam giác/main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include <cmath>
 using namespace std;
 
-#define int long long
-const int MAXN = 1e3 + 7;
-int a[MAXN], b[MAXN], c[MAXN];
-int n, ans;
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
 
 signed main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n;
-    for(int i = 1; i <= n; ++i) cin >> a[i];
-    for(int i = 1; i <= n; ++i) cin >> b[i];
-    for(int i = 1; i <= n; ++i) cin >> c[i];
-    sort(c + 1, c + n + 1);
-    for(int i = 1; i <= n; ++i)
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(0);
+    while(test--)
     {
-        for(int j = 1; j <= n; ++j)
+        int n; cin >> n;
+        int ans = 0;
+        vector <int> a(n);
+        vector <int> b(n);
+        vector <int> c(n);
+        for(int i = 0; i < n; ++i) cin >> a[i];
+        for(int i = 0; i < n; ++i) cin >> b[i];
+        for(int i = 0; i < n; ++i) cin >> c[i];
+        sort(c.begin(), c.end());
+        for(int i = 0; i < n; ++i)
         {
-            int k1 = upper_bound(c + 1, c + n + 1, abs(a[i] - b[j])) - c;
-            int k2 = lower_bound(c + 1, c + n + 1, (a[i] + b[j])) - c - 1;
-            if(k1 <= k2) ans += (k2 - k1 + 1);
+            for(int j = 0; j < n; ++j)
+            {
+                int k1 = upper_bound(c.begin(), c.end(), abs(a[i] - b[j])) - c.begin();
+                int k2 = lower_bound(c.begin(), c.end(), a[i] + b[j]) - c.begin() - 1;
+                if(k1 <= k2) ans += k2 - k1 + 1;
+            }
         }
+        cout << ans << '\n';
     }
-    cout << ans << '\n';
     return 0;
 }
