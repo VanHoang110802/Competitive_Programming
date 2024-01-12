debf1b8fd8406d15a47f236e557470308255f349
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-12 16:29:21 +0700

    Create main.cpp

---
+++ Xử Lý Số lớn/Tam giác số/main.cpp
@@ -0,0 +1,7 @@
+/*
+Đánh số các hàng từ 1 đên N theo chiều từ trên xuống, các số trên hàng i đánh số từ 1 đên i theo chiều từ trái qua phải.
+Vị trí thứ j trên hàng i được gọi là vị trí (i; j). 
+Từ (i; j) có thể đi đến (i+1; j) và (i+1; j+1).
+Gọi lMax(i,j) và lMin(i,j) tương ứng là hai nhãn giá trị lớn nhất và nhỏ nhất đạt được khi đi từ đỉnh (1; 1) đến (i; j). 
+Hai giá trị nhãn của (i; j) có thể chuyển cho (i+1; j) và (i+1; j+1). Phải xử lý số lớn có dấu.
+*/
