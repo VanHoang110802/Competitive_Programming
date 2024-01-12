90d850fd1c350e142b2ba08ee630189163adc711
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-12 10:38:40 +0700

    Update main.cpp

--- Xử Lý Số lớn/Phép toán với số nguyên lớn/main.cpp
+++ Xử Lý Số lớn/Phép toán với số nguyên lớn/main.cpp
@@ -170,7 +170,7 @@ string mod(string X, string Y)
 
 signed main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(0);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1);
     while(test--)
     {
         cin >> A >> B;
