318b723d6b94a16f898d79984bffc7efcefa7bdd
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-23 11:30:19 +0700

    Create main.cpp

---
+++ Duyệt trâu - Tham lam/In ma trận 3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+#define int long long
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
+
+signed main()
+{
+    //freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
+    while(test--)
+    {
+        int n, m; cin >> n >> m;
+        for(int i = 1; i <= n; ++i)
+        {
+            for(int j = 1; j <= m; ++j)
+            {
+                cout << (j - 1) * n + i << ' ';
+            }
+            cout << '\n';
+        }
+    }
+    cerr << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
