d70ab447615893d94eeb2ca5c3c263766d7acab3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-23 11:51:53 +0700

    Create main.cpp

---
+++ Xử Lý Số lớn/Số T - Nguyên Tố/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+#define int long long
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
+
+bool CheckNguyenTo(int n)
+{
+    for(int i = 2; i * i <= n; ++i) if(n % i == 0) return false;
+    return (n > 1);
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
+    while(test--)
+    {
+        int n, cnt = 0;
+        cin >> n;
+        for(int i = 1; i * i <= n; ++i) if(CheckNguyenTo(i)) cnt++;
+
+        cout << cnt << '\n';
+    }
+    cerr << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
