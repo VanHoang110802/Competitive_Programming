9ea9f4d6f368723d299da93f656cc312a6307651
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-29 17:32:04 +0700

    Update main.cpp

--- Xử Lý Số lớn/Đèn Nhấp Nháy/main.cpp
+++ Xử Lý Số lớn/Đèn Nhấp Nháy/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define int long long
+const double esp = 1e-18;
 int test;
 
 void CinTest(int vanhoang)
@@ -11,13 +12,19 @@ void CinTest(int vanhoang)
     else test = 1;
 }
 
+int CheckNguyen(double x)
+{
+    if((x - (int)x) < esp) return x;
+    return x++;
+}
+
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
     while(test--)
     {
         int p, q, n; cin >> p >> q >> n;
-        cout << round(sqrt(q - p)) - 1 << '\n';
+        cout << CheckNguyen(sqrt(q)) - CheckNguyen(sqrt(p - 1)) << '\n';
     }
     return 0;
 }
