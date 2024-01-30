e1ef865562143fbb31633b3255b84432d30783d7
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-30 13:45:53 +0700

    Update main.cpp

--- Prefix sum/Bài 1/main.cpp
+++ Prefix sum/Bài 1/main.cpp
@@ -15,9 +15,11 @@ signed main()
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
     while(test--)
     {
-        ull q, k, l, r; cin >> q >> k >> l >> r;
+        ull q, k; cin >> q >> k;
         while(q--)
         {
+             ull l, r;
+             cin >> l >> r;
              cout << (r / k) - ((l - 1)/k) << '\n';
         }
     }
