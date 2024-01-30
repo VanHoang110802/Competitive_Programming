65e3caac3e6758a9746b2839e72916ae546a02a9
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-30 11:09:16 +0700

    Create main.cpp

---
+++ Prefix sum/Bài 1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
+    while(test--)
+    {
+        int q, k, l, r; cin >> q >> k >> l >> r;
+        while(q--)
+        {
+             cout << (r / k) - ((l - 1)/k) << '\n';
+        }
+    }
+    return 0;
+}
