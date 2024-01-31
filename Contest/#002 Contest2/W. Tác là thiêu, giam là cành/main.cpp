14c00897666a0d66905b93f3cd00fcf5d7e7594b
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-31 12:06:24 +0700

    Create main.cpp

---
+++ Contest/#002 Contest2/W. Tác là thiêu, giam là cành/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+#define int long long
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
+    while(test--)
+    {
+        int n; cin >> n;
+        for(int i = 1; i <= n; ++i)
+        {
+            for(int j = 1; j <= 2 * n - 1; ++j)
+            {
+                if(i - j == 1 - n || i + j == n + 1 || i == n) cout << '*';
+                else cout << ' ';
+            }
+            cout << '\n';
+        }
+    }
+    cerr << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
