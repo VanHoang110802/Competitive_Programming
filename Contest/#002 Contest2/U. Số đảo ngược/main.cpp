1e1aca07223a0bbe6e8d6f62b9a290b1aaad1fa6
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-01-31 12:04:07 +0700

    Create main.cpp

---
+++ Contest/#002 Contest2/U. Số đảo ngược/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+#define int long long
+int test;
+
+void CinTest(int vanhoang)
+{
+    if(vanhoang > 0) cin >> test;
+    else test = 1;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); CinTest(1 - 1);
+    while(test--)
+    {
+        string s; cin >> s;
+        string temp = s;
+        reverse(s.begin(), s.end());
+        cout << s << '\n';
+    }
+    cerr << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
