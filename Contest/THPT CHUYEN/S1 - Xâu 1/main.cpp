26356eaf0269d9da47f0bd11897d49ed38964179
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-02-07 18:11:30 +0700

    Create main.cpp

---
+++ Contest/THPT CHUYEN/S1 - Xâu 1/main.cpp
@@ -0,0 +1,31 @@
+// http://thptchuyen.ntucoder.net/Problem/Details/4475
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string s;
+
+void XuLyNhanh()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+
+void Nhap()
+{
+    cin >> s;
+}
+
+void GiaiBai()
+{
+    int n = s.size();
+    cout << n;
+}
+
+int main()
+{
+    XuLyNhanh();
+    Nhap();
+    GiaiBai();
+    return 0;
+}
