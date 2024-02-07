f9f646f1513300b3f2f73a91f8b1cd034cd94404
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-02-07 18:10:20 +0700

    Create main.cpp

---
+++ Contest/THPT CHUYEN/CAL3 - Tính toán 3/main.cpp
@@ -0,0 +1,41 @@
+// http://thptchuyen.ntucoder.net/Problem/Details/4474
+
+#include<bits/stdc++.h>
+using namespace std;
+
+long long n;
+
+void XuLyNhanh()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+
+//void File()
+//{
+//    if (fopen("name.INP", "r"))
+//    {
+//        freopen("name.INP", "r", stdin);
+//        freopen("name.OUT",  "w", stdout);
+//    }
+//}
+
+void Nhap()
+{
+    cin >> n;
+}
+
+void GiaiBai()
+{
+    long long s = n*(n+1) / 2;
+    cout << s;
+}
+
+int main()
+{
+    XuLyNhanh();
+    //File();
+    Nhap();
+    GiaiBai();
+    return 0;
+}
