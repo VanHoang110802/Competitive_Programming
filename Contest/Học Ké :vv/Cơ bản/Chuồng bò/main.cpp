7455eea0ef22aaee289956b53ca67415aa08c775
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-02-22 09:01:47 +0700

    Create main.cpp

---
+++ Contest/Học Ké :vv/Cơ bản/Chuồng bò/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+#define int long long
+const int MAXN = 1e6 + 7;
+int a[MAXN], cnt[MAXN]
+
+signed main()
+{
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n; cin >> n;
+  
+  for(int i = 1; i <= n; ++i)
+  {
+    cin >> a[i];
+    cnt[a[i] + MAXN]++;
+  }
+  int d = 0;
+  for(int i = 0; i <= 2 * MAXN; ++i)
+  {
+    if(cnt[i] > 0) d++;
+  }
+  cout << d << '\n';
+  for(int i = 0; i <= 2 * MAXN; ++i)
+  {
+    if(cnt[i] > 0) cout << i - MAXN << ' ' << c[i] << '\n';
+  }
+  return 0;
+}
