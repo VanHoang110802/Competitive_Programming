d36c3113d2ec9a84d88f475c9b7a5204db1b58c2
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-02-23 18:47:34 +0700

    Update main.cpp

--- Contest/Học Ké :vv/Duyệt mảng/Hàng cây của nữ hoàng/main.cpp
+++ Contest/Học Ké :vv/Duyệt mảng/Hàng cây của nữ hoàng/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <cstring>
 using namespace std;
 
 const int MAXN = 1e3 + 7;
-int a[MAXN], b[MAXN];
-vector<int> temp, res;
+int a[MAXN], b[MAXN], temp[MAXN], res[MAXN];
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n, k, MIN = 1e9; cin >> n >> k;
     for(int i = 1; i <= n; ++i) cin >> a[i];
-    res.resize(n + 3);
-    temp.resize(n + 3);
+    memset(res, 0, sizeof(res));
+
     for(int i = 1; i <= 1000; ++i)
     {
         b[1] = i;
@@ -22,19 +20,23 @@ signed main()
             b[j] =  b[j - 1] + k;
         }
         int cnt = 0;
-        fill(temp.begin(), temp.end(), 0);
-        for(int i = 1; i <= n; ++i)
+        memset(temp, 0, sizeof(temp));
+
+        for(int j = 1; j <= n; ++j)
         {
-            if(b[i] != a[i])
+            if(b[j] != a[j])
             {
                 cnt++;
-                temp[i] = b[i] - a[i];
+                temp[j] = b[j] - a[j];
             }
         }
         if(cnt < MIN)
         {
             MIN = cnt;
-            res = temp;
+            for(int j = 1; j <= n; ++j)
+            {
+                res[j] = temp[j];
+            }
         }
     }
     cout << MIN << '\n';
