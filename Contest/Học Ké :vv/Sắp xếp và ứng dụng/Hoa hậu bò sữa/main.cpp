71e55dd7414d165315b391d67bc25e2a120e01e9
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-02-27 14:01:06 +0700

    Update main.cpp

--- Contest/Học Ké :vv/Sắp xếp và ứng dụng/Hoa hậu bò sữa/main.cpp
+++ Contest/Học Ké :vv/Sắp xếp và ứng dụng/Hoa hậu bò sữa/main.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 #define int long long
 #define fi first
 #define se second
 const int MAXN = 1e6 + 7;
-int a[MAXN], b[MAXN], c[MAXN];
-
-bool cmp(int a, int b)
-{
-    int ans1 = a % 2, ans2 = b % 2;
-    if(ans1 == 0 && ans2 == 1) return false;
-    else if(ans1 == 1 && ans2 == 0) return true;
-    if(ans1 == 0 && ans2 == 0) return (a > b);
-    else if(ans1 == 1 && ans2 == 1) return (a < b);
-}
+int a[MAXN], b[MAXN], c[MAXN], f[MAXN];
 
 signed main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n, m; cin >> n >> m;
-    for(int i = 1; i <= n; ++i) cin >> a[i];
-    sort(a + 1, a + n + 1, cmp);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> f[i];
+        if(f[i] == 1) a[i]++;
+        else if(f[i] == 2) b[i]++;
+        else if(f[i] == 3) c[i]++;
+
+        a[i] = a[i - 1] + a[i];
+        b[i] = b[i - 1] + b[i];
+        c[i] = c[i - 1] + c[i];
+    }
     while(m--)
     {
         int l, r;
         cin >> l >> r;
-        int vang = 0, bac = 0, dong = 0;
-        for(int i = l; i <= r; ++i)
-        {
-            if(a[i] == 1) vang++;
-            else if(a[i] == 2) bac++;
-            else if(a[i] == 3) dong++;
-        }
-        cout << vang << ' ' << bac << ' ' << dong << '\n';
+        cout << a[r] - a[l - 1] << ' ' << b[r] - b[l - 1] << ' ' << c[r] - c[l - 1] << '\n';
     }
     return 0;
 }
