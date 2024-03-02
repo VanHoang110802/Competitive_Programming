50c7b371110c4de7c4818e6321137c4e4e128cb2
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-02 13:29:12 +0700

    Create main.cpp

---
+++ Contest/#019 Contest19/Sereja and Dima/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#define int long long
+const int MAXN = 1e3 + 7;
+int a[MAXN];
+int n, sum;
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    int l = 1, r = n, s = 0;
+    while(l <= r)
+    {
+        if(a[l] > a[r])
+        {
+            s += a[l];
+            l++;
+        }
+        else
+        {
+            s += a[r];
+            r--;
+        }
+        if(a[l] > a[r])
+        {
+            l++;
+        }
+        else
+        {
+            r--;
+        }
+    }
+    cout << s << ' ' << sum - s << '\n';
+    return 0;
+}
