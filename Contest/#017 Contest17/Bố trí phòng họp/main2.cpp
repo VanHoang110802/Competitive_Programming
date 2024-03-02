b76bd7e9e40ac9e469facc6371da6197b7aa8642
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-02 13:08:59 +0700

    Create main2.cpp

---
+++ Contest/#017 Contest17/Bố trí phòng họp/main2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define int long long
+const int MAXN = 1e6 + 7;
+int a[MAXN], b[MAXN], F[MAXN];
+int n;
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i] >> b[i];
+    sort(a + 1, a + n + 1);
+    sort(b + 1, b + n + 1);
+    F[1] = 1;
+    for(int i = 2; i <= n; ++i)
+    {
+        F[i] = 1;
+        for(int j = 1; j <= i - 1; ++j)
+        {
+            if(b[j] <= a[i])
+            {
+                F[i] = max(F[i], F[j] + 1);
+            }
+        }
+    }
+    int ans = -1;
+    for(int i = 1; i <= n; ++i)
+    {
+        ans = max(F[i], ans);
+    }
+    cout << ans << '\n';
+    return 0;
+}
