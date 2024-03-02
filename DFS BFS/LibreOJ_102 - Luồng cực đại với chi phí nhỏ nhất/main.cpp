fac793b258c34689614bd0139ba9e1cb19392607
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-02 12:51:04 +0700

    Update main.cpp

--- DFS BFS/LibreOJ_102 - Luồng cực đại với chi phí nhỏ nhất/main.cpp
+++ DFS BFS/LibreOJ_102 - Luồng cực đại với chi phí nhỏ nhất/main.cpp
@@ -4,24 +4,33 @@
 #define INF 1234567891011
 #define ll long long
 using namespace std;
+
 int n, m, S, T, head[N], cur[N], cnt;
 ll dis[N], ans1, ans2;
 bool bz[N];
-struct node {
+struct node
+{
     int to, nxt;
     ll w, c;
 } e[M << 1];
-void add(int u, int v, ll w, ll c) {
-    e[++cnt] = (node) {
+
+void add(int u, int v, ll w, ll c)
+{
+    e[++cnt] = (node)
+    {
         v, head[u], w, c
     };
     head[u] = cnt;
 }
-void adde(int u, int v, ll w, ll c) {
+
+void adde(int u, int v, ll w, ll c)
+{
     add(u, v, w, c);
     add(v, u, 0, -c);
 }
-bool bfs() {
+
+bool bfs()
+{
     for (int i = 1; i <= n; ++i)
         dis[i] = INF;
 
@@ -30,15 +39,18 @@ bool bfs() {
     dis[S] = 0;
     bz[S] = 1;
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int u = q.front();
         q.pop();
         bz[u] = 0;
 
-        for (int i = head[u]; ~i; i = e[i].nxt) {
+        for (int i = head[u]; ~i; i = e[i].nxt)
+        {
             int v = e[i].to;
 
-            if (e[i].w && dis[v] > dis[u] + e[i].c) {
+            if (e[i].w && dis[v] > dis[u] + e[i].c)
+            {
                 dis[v] = dis[u] + e[i].c;
 
                 if (!bz[v])
@@ -49,18 +61,22 @@ bool bfs() {
 
     return dis[T] != INF;
 }
-ll dfs(int u, ll maxflow) {
+
+ll dfs(int u, ll maxflow)
+{
     if (u == T)
         return maxflow;
 
     ll flow = 0;
     bz[u] = 1;
 
-    for (int i = cur[u]; ~i; i = e[i].nxt) {
+    for (int i = cur[u]; ~i; i = e[i].nxt)
+    {
         cur[u] = i;
         int v = e[i].to;
 
-        if (!bz[v] && dis[v] == dis[u] + e[i].c && e[i].w) {
+        if (!bz[v] && dis[v] == dis[u] + e[i].c && e[i].w)
+        {
             ll delta = dfs(v, min(maxflow - flow, e[i].w));
             e[i].w -= delta;
             e[i ^ 1].w += delta;
@@ -75,22 +91,28 @@ ll dfs(int u, ll maxflow) {
     bz[u] = 0;
     return flow;
 }
-void dinic() {
-    while (bfs()) {
+
+void dinic()
+{
+    while (bfs())
+    {
         for (int i = 1; i <= n; ++i)
             cur[i] = head[i];
 
         ans1 += dfs(S, INF);
     }
 }
-int main() {
+
+int main()
+{
     scanf("%d%d", &n, &m);
     S = 1;
     T = n;
     memset(head, -1, sizeof(head));
     cnt = -1;
 
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= m; ++i)
+    {
         int u, v;
         ll w, c;
         scanf("%d%d%lld%lld", &u, &v, &w, &c);
