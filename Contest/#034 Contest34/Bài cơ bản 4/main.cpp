3fdc805e8d2cd7d66ef25ad34a725a652d4f7142
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-03 18:08:39 +0700

    Create main.cpp

---
+++ Contest/#034 Contest34/Bài cơ bản 4/main.cpp
@@ -0,0 +1,19 @@
+//  Hàm tính căn bậc 3 của 1 số thực có thể cài đặt đệ quy theo 2 hàm exp và ln
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float DeQuy(float x)
+{
+	if(x == 0) return 0;
+	if(x < 0) return (-DeQuy(-x));
+	return (exp((log(x)/3)));
+}
+
+int main()
+{
+    int n = 27;
+    cout << DeQuy(n);
+    return 0;
+}
