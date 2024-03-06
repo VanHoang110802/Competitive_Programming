d7325dfce5d689f09a5b2b79cde9a981daabd89b
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-06 11:45:29 +0700

    Update main.cpp

--- Contest/#022 Contest22/Giá trị max từ 1 đến p/main.cpp
+++ Contest/#022 Contest22/Giá trị max từ 1 đến p/main.cpp
@@ -1,29 +1,31 @@
-
 #include <iostream>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 #define int long long
 const int MAXN = 1e6 + 7;
-int a[MAXN];
+int a[MAXN], MAX[MAXN];
 int n, q;
 
 void Nhap()
 {
+	memset(MAX, -1e18, sizeof MAX);
 	cin >> n >> q;
-	for (int i = 1; i <= n; ++i) cin >> a[i];
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> a[i];
+		MAX[i] = max(MAX[i - 1], a[i]);
+	}
 }
 
 void XuLy()
 {
-	while (q--) // O(q)
+	while (q--) 
 	{
-		int x, MAX = -1e18; cin >> x;
-		for (int i = 1; i <= x; ++i) // O(x)
-		{
-			if (a[i] > MAX) MAX = a[i];
-		}
-		cout << MAX << '\n';
+		int x;
+		cin >> x;
+		cout << MAX[x] << '\n';
 	}
 }
 
