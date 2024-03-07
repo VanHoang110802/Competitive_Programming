096950bde10c68b4855c64aaefbf00fb3495ac7b
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-07 13:04:47 +0700

    Create main.cpp

---
+++ Contest/#037 Contest37/Đếm tần suất xuất hiện của từng giá trị trong mảng và liệt kê theo thứ tự từ nhỏ tới lớn/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 1e6 + 7;
+int a[MAXN];
+int n;
+
+void Nhap()
+{
+	cin >> n;
+	for (int i = 1; i <= n; ++i) cin >> a[i];
+}
+
+void XuLy()
+{
+	sort(a + 1, a + n + 1);
+	int dem = 1; // a[0];
+	for (int i = 2; i <= n; i++) 
+	{
+		if (a[i] == a[i - 1])
+		{
+			++dem;
+		}
+		else 
+		{
+			cout << a[i - 1] << " xuat hien " << dem << " lan\n";
+			dem = 1;
+		}
+	}
+	cout << a[n - 1] << " xuat hien " << dem << " lan\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	Nhap();
+	XuLy();
+	return 0;
+}
