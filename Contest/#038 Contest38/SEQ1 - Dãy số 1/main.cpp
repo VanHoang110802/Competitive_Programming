9389aaaaf21cb12251fa54c71d054fc21564b0b5
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-08 18:10:35 +0700

    Update main.cpp

--- Contest/#038 Contest38/SEQ1 - Dãy số 1/main.cpp
+++ Contest/#038 Contest38/SEQ1 - Dãy số 1/main.cpp
@@ -17,7 +17,6 @@ int main() {
     return 0;
 }
 */
-
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -29,16 +28,19 @@ int n, maxDiff = -1e9;
 
 signed main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n; cin >> n;
-	for(int i = 1; i <= n; ++i) cin >> a[i];
-	memset(dp, 1e9, sizeof dp);
-	dp[1] = a[1];
-	for(int i = 2; i <= n; ++i)
-	{
-		    dp[i] = min(dp[i - 1], a[i]);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+    memset(dp, 1e9, sizeof dp);
+    dp[1] = a[1];
+    for(int i = 2; i <= n; ++i)
+    {
+        dp[i] = min(dp[i - 1], a[i]);
         maxDiff = max(maxDiff, a[i] - dp[i]);
-	}
-	cout << maxDiff << '\n';
+    }
+    cout << maxDiff << '\n';
     return 0;
 }
