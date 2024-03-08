ffd73e499878327754dcfbaa63946faa80398961
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-08 13:44:11 +0700

    Create main.cpp

---
+++ Contest/#022 Contest22/ tìm k số liên tiếp có tổng lớn nhất/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define int long long
+const int MAXN = 1e6 + 7;
+int a[MAXN];
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+    int i = 1, j = 1, sum = 0, cnt = 0, MAX = -1e18, vitri = 0;
+    while(i <= n && j <= n)
+    {
+        if(cnt < k)
+        {
+            sum += a[j];
+            ++j;
+            cnt++;
+        }
+        else if(cnt == k)
+        {
+            cout << i << '\n';
+            if(sum > MAX)
+            {
+                vitri = i - 1;
+                MAX = sum;
+            }
+            ++i; --j;
+            sum = a[i];
+            cnt = 1;
+            //break;
+        }
+    }
+    cout << MAX << '\n';
+    for(int i = 1; i <= k; ++i) cout << a[i + vitri] << ' ';
+    //int a, b, c; cin >> a >> b >> c;
+    //cout << a + b + c << '\n';
+    return 0;
+}
+
