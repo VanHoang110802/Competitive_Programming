ca59782781a58d89b1b8f4daca1cd4da1ba3ca06
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-08 18:00:15 +0700

    Create main.cpp

---
+++ Contest/#038 Contest38/SEQ1 - Dãy số 1/main.cpp
@@ -0,0 +1,44 @@
+/*
+#include <iostream>
+#define lli long long int
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    lli a[n]; cin >> a[0];
+    lli smallest = a[0], maxDiff = INT_MIN;
+    for (int i = 1; i < n; i++) {
+        cin >> a[i];
+        smallest = min(smallest, a[i]);
+        maxDiff = max(maxDiff, a[i] - smallest);
+        cout << a[i] - smallest << '\n';
+    }
+    cout << smallest << ' ' << maxDiff << endl;
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define int long long
+const int MAXN = 1e6 + 7;
+int a[MAXN], dp[MAXN];
+int n, maxDiff = -1e9;
+
+signed main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n; cin >> n;
+	for(int i = 1; i <= n; ++i) cin >> a[i];
+	memset(dp, 1e9, sizeof dp);
+	dp[1] = a[1];
+	for(int i = 2; i <= n; ++i)
+	{
+		    dp[i] = min(dp[i - 1], a[i]);
+        maxDiff = max(maxDiff, a[i] - dp[i]);
+	}
+	cout << maxDiff << '\n';
+    return 0;
+}
