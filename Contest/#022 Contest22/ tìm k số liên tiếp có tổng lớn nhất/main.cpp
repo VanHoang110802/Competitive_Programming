21abe169b78aa071521aa3f0747546f564726452
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-10 19:07:00 +0700

    Update main.cpp

--- Contest/#022 Contest22/ tìm k số liên tiếp có tổng lớn nhất/main.cpp
+++ Contest/#022 Contest22/ tìm k số liên tiếp có tổng lớn nhất/main.cpp
@@ -5,35 +5,31 @@ using namespace std;
 #define int long long
 const int MAXN = 1e6 + 7;
 int a[MAXN];
+int n, k;
+
+void Nhap()
+{
+    cin >> n >> k;
+    for(int i = 0; i < n; ++i) cin >> a[i];
+}
+
+void XuLy()
+{
+    int max_sum = 0;
+    for(int i = 0; i < k; i++) max_sum += a[i];
+    int window_sum = max_sum;
+    for(int i = k; i < n; i++)
+    {
+        window_sum += a[i] - a[i - k];
+        max_sum = max(max_sum, window_sum);
+    }
+    cout << max_sum << '\n';
+}
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n, k;
-    cin >> n >> k;
-    for(int i = 1; i <= n; ++i) cin >> a[i];
-    int i = 1, j = 1, sum = 0, cnt = 0, MAX = -1e18, vitri = 0;
-    while(i <= n && j <= n)
-    {
-        if(cnt < k)
-        {
-            sum += a[j];
-            ++j;
-            cnt++;
-        }
-        else if(cnt == k)
-        {
-            if(sum > MAX)
-            {
-                vitri = i;
-                MAX = sum;
-            }
-            ++i; --j;
-            sum = a[i];
-            cnt = 1;
-        }
-    }
-    cout << MAX << '\n';
-    for(int i = 1; i <= k; ++i) cout << a[i + vitri - 1] << ' ';
+    Nhap();
+    XuLy();
     return 0;
 }
