1bb2a683685074c75aaf5d04b53045e4cf2a768f
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-11 18:28:40 +0700

    Update main.cpp

--- Contest/#022 Contest22/Tích lớn nhất của 3 phần tử riêng biệt/main.cpp
+++ Contest/#022 Contest22/Tích lớn nhất của 3 phần tử riêng biệt/main.cpp
@@ -9,23 +9,49 @@ int n;
 
 void Nhap()
 {
-	cin >> n;
-	for (int i = 1; i <= n; ++i) cin >> a[i];
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
 }
 
 void XuLy()
 {
-	sort(a + 1, a + n + 1); // O(nlogn)
-	int ans_1 = a[1] * a[2] * a[n];
-	int ans_2 = a[n] * a[n - 1] * a[n - 2];
-	if ((a[1] < 0 && a[2] < 0) && (ans_1 > ans_2)) cout << ans_1 << '\n';
-	else cout << ans_2 << '\n';
+    int max_1 = -1e18, max_2 = 1e18, max_3 = 1e18, min_1 = 1e18, min_2 = 1e18;
+    for(int i = 1; i <= n; ++i)
+    {
+        if(a[i] > max_1)
+        {
+            max_3 = max_2;
+            max_2 = max_1;
+            max_1 = a[i];
+        }
+        else if(a[i] > max_2 && a[i] != max_1)
+        {
+            max_3 = max_2;
+            max_2 = a[i];
+        }
+        else if(a[i] > max_3 && a[i] != max_1 && a[i] != max_2)
+        {
+            max_3 = a[i];
+        }
+        if(a[i] < min_1)
+        {
+            min_2 = min_1;
+            min_1 = a[i];
+        }
+        else if(a[i] < min_2 && a[i] != min_1)
+        {
+            min_2 = a[i];
+        }
+    }
+    cout << max(min_1 * min_2 * max_1, max_1 * max_2 * max_3);
 }
 
 signed main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	Nhap();
-	XuLy();
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    Nhap();
+    XuLy();
+    return 0;
 }
