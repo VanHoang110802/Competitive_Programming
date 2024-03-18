b30705dab1a130e8c012747cc1070bb3e144e890
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-18 15:15:07 +0700

    Update main.cpp

--- Contest/#022 Contest22/sắp xếp các số nguyên tố giảm dần ,và các số hoàn chỉnh tăng dần theo những  vị trí/main.cpp
+++ Contest/#022 Contest22/sắp xếp các số nguyên tố giảm dần ,và các số hoàn chỉnh tăng dần theo những  vị trí/main.cpp
@@ -4,35 +4,36 @@
 
 23 6 28 13 7 10 5 28 3 2
 
+10
+8128 13 15 59 66 28 9 73 12 17
+
+6
+5 6 5 28 5 6
 */
 
-// C++ implementation of the approach
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
 using namespace std;
 
 #define int long long
 const int N = 1e6 + 7;
 bool prime[N];
-const int z = 1e18 + 7;
-int perfect[N];
+int arr[N], perfect[N];
 int n;
-int arr[N];
 map<int, bool> MAP;
 vector<int> v1, v2;
-
-struct node
-{
-    int temp1, temp2;
-}
-func[N];
 
 int POW(int x, int y)
 {
     if (y == 0) return 1;
     if (x == 0) return 0;
     int temp = POW(x, y / 2);
-    if (y % 2 == 0)	return (temp % z) * (temp % z) % z;
-    else return (temp % z) * (temp % z) % z * (x % z) % z;
+    if (y % 2 == 0)	return (temp * temp);
+    else return (temp * temp * x);
 }
 
 void SieveOfEratosthenes()
@@ -57,15 +58,12 @@ void SangSoHoanHao()
     {
         if(prime[i])
         {
-            //cout << POW(2, i - 1) * (POW(2, i) - 1) << '\n';
-            //perfect[idx] = POW(2, i - 1) * (POW(2, i) - 1);
             ans = POW(2, i - 1) * (POW(2, i) - 1);
             MAP[ans] = true;
         }
-        if(ans > z) break;
+        if(ans > 1e18) break;
     }
 }
-
 
 void SortArray()
 {
@@ -73,24 +71,10 @@ void SortArray()
     {
         if(MAP[arr[i]]) v1.push_back(arr[i]);
         if(prime[arr[i]]) v2.push_back(arr[i]);
-        //if(MAP[arr[i]])
-        //{
-            //func[i].temp1 = arr[i];
-            //cout << func[i].temp1 << '\n';
-            //func[i].temp2 = i;
-        //}
-        //if(prime[arr[i]])
-        //{
-            //func[i].temp1 = arr[i];
-            //func[i].temp2 = i;
-        //}
     }
-    //for(int i = 1; i <= n; ++i) cout << func[i].temp1 << ' ' << func[i].temp2 << '\n';
-    //sort(func + 1, func + n + 1, cmp); // O(N * log(N))
-    //for(int i = 1; i <= n; ++i) cout << func[i].temp1 << ' ';
 
     sort(v2.begin(), v2.end(), greater<int>()); // O(N * log(N))
-    sort(v1.begin(), v1.end()); // O(N * log(N))
+    sort(v1.begin(), v1.end());                 // O(N * log(N))
 
     int j = 0, k = 0;
     for(int i = 1; i <= n; ++i) // O(N)
@@ -103,8 +87,6 @@ void SortArray()
 
         cout << arr[i] << ' ';
     }
-
-
 }
 
 signed main()
@@ -114,14 +96,7 @@ signed main()
     cin >> n;
     for(int i = 1; i <= n; ++i) cin >> arr[i];
     SortArray();
+    //cout << 2 * (N * log(N));
     return 0;
 }
-// O((n * log n) + (100005) 3/2 )
 
-// O(N*log(log(N))) + O(sqrt(N)) + O(N)
-// ~3 * 1e6
-// ~1e6
-/*
-10
-1 2 3 4 5 6 7 8 9 10
-*/
