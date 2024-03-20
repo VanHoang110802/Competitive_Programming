b09558191fa1ace3c7c1f8288a3a5e473b4671dc
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-20 10:19:22 +0700

    Update main.cpp

--- Contest/#022 Contest22/ tìm k số liên tiếp có tổng lớn nhất/main.cpp
+++ Contest/#022 Contest22/ tìm k số liên tiếp có tổng lớn nhất/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 #define int long long
 const int MAXN = 1e6 + 7;
 int a[MAXN];
-int n, k, sum = 0, idx = 0;
+int n, k, sum = 0, idx = 1;
 
 void Nhap()
 {
@@ -15,25 +16,30 @@ void Nhap()
 
 void XuLy()
 {
-    for(int i = 1; i <= k; i++) sum += a[i];
+    for (int i = 1; i <= k; ++i) sum += a[i];
+
     int ans = sum;
-    for(int i = k + 1; i <= n; i++)
+    for (int i = k + 1; i <= n; ++i)
     {
         ans += a[i] - a[i - k];
-        if(ans >= sum)
+        if (ans > sum)
         {
             sum = ans;
-            idx = i;
+            idx = i - k + 1;
         }
     }
+
     cout << sum << '\n';
-    for(int i = 1; i <= k; ++i) cout << a[i + k + 1] << ' ';
+    for (int i = idx; i <= idx + k - 1; ++i) cout << a[i] << ' ';
 }
 
 signed main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
     Nhap();
     XuLy();
+    //cout << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
     return 0;
 }
