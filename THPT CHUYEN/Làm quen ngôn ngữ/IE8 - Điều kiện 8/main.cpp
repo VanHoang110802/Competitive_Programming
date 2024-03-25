a8223c1cb3ac0215850537cdd4eaaf62b920808c
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-25 14:40:09 +0700

    Create main.cpp

---
+++ THPT CHUYEN/Làm quen ngôn ngữ/IE8 - Điều kiện 8/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define int long long
+
+void XuLy()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if((a / b == c) || (b / c == a) || (c / a == b)) cout << '/';
+    else cout << "NOSOL";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    XuLy();
+    cout << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
