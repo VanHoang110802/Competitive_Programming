ea0f0700f02d32a34870c38bec5022617ae1cb5e
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-25 13:39:54 +0700

    Create main.cpp

---
+++ THPT CHUYEN/Làm quen ngôn ngữ/S2 - Xâu 2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+#define int long long
+
+void XuLy()
+{
+    string s; cin >> s;
+    char x; cin >> x;
+    int n = s.size(), cnt = 0;
+    for(int i = 0; i < n; ++i) if(s[i] == x) cnt++;
+    cout << cnt << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    XuLy();
+    cout << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
