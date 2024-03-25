be4c7a78e8718434a8b21ef280fd615dd0048be8
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-25 14:32:40 +0700

    Create main.cpp

---
+++ THPT CHUYEN/Làm quen ngôn ngữ/IE3 - Điều kiện 3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+#define int long long
+
+void XuLy()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << min({a, b, c, d});
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    XuLy();
+    cout << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
