52d5ee948b4fba5e0dbefc810dff34422110a2dd
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-03-27 15:50:08 +0700

    Create main.cpp

---
+++ SERIES CONTEST NO TEST/2019/SPRING CONTEST/DAY 1/BAI 2/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+const int MAX_N = 105;
+const int MAX_N1 = 105;
+const int MAX_N2 = 105;
+const int MAX_M = 105 * 105;
+int a[MAX_N][MAX_N];
+class hopcrofkarp
+{
+private:
+    int n1, n2, edges, last[MAX_N1], prev[MAX_M], head[MAX_M];
+    int matching[MAX_N2], dist[MAX_N1], Q[MAX_N1];
+    bool used[MAX_N1], vis[MAX_N1];
+
+public:
+    void init(int _n1, int _n2)
+    {
+        n1 = _n1;
+        n2 = _n2;
+        edges = 0;
+        fill(last, last + n1, -1);
+    }
+
+    void addEdge(int u, int v)
+    {
+        head[edges] = v;
+        prev[edges] = last[u];
+        last[u] = edges++;
+    }
+
+    void bfs()
+    {
+        fill(dist, dist + n1, -1);
+        int sizeQ = 0;
+        for (int u = 0; u < n1; ++u)
+        {
+            if (!used[u])
+            {
+                Q[sizeQ++] = u;
+                dist[u] = 0;
+            }
+        }
+
+        for (int i = 0; i < sizeQ; i++)
+        {
+            int u1 = Q[i];
+            for (int e = last[u1]; e >= 0; e = prev[e])
+            {
+                int u2 = matching[head[e]];
+                if (u2 >= 0 && dist[u2] < 0)
+                {
+                    dist[u2] = dist[u1] + 1;
+                    Q[sizeQ++] = u2;
+                }
+            }
+        }
+    }
+
+    bool dfs(int u1)
+    {
+        vis[u1] = true;
+        for (int e = last[u1]; e >= 0; e = prev[e])
+        {
+            int v = head[e];
+            int u2 = matching[v];
+            if (u2 < 0 || !vis[u2] && dist[u2] == dist[u1] + 1 && dfs(u2))
+            {
+                matching[v] = u1;
+                used[u1] = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int maxMatching()
+    {
+        fill(used, used + n1, false);
+        fill(matching, matching + n2, -1);
+        for (int res = 0;;)
+        {
+            bfs();
+            fill(vis, vis + n1, false);
+            int f = 0;
+            for (int u = 0; u < n1; ++u)
+                if (!used[u] && dfs(u)) ++f;
+            if (!f) return res;
+            res += f;
+        }
+    }
+};
+
+void XuLy()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    int res = 0;
+
+    for (int val = -n; val <= n; val++)
+    {
+        hopcrofkarp HK;
+        HK.init(n + 3, n + 3);
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (val == a[i][j])
+                {
+                    HK.addEdge(i, j);
+                }
+            }
+        }
+
+        res += HK.maxMatching();
+    }
+
+    cout << n * n - res << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int test;
+    cin >> test;
+    while(test--)
+    {
+        XuLy();
+    }
+    cout << "\nTime elapsed: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms.\n";
+    return 0;
+}
